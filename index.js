066a63422a582825b8900bbf49a7a6e551affdf8
Oscar Andres Caicedo Bedoya <oscar.caicedo@frubana.com>
2022-11-19 07:52:03 -0500

    rest api

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const body_parser = require("body-parser")
 const app = express()
 const path =  "/vuelos"
 const portvuelos = 8081
-//const portcliente = 8082
-//const portpago = 8083
-//const portreserva = 8084
 
 app.use(cors())
 app.use(body_parser.json())
